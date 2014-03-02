fbb1ec9e42e4f9d41b680ebf95a317f25d4331c0
George Barnick <george.barnick@brickcraft.me>
2014-03-02 17:46:36 -0500

    Adding German (Deutsch) internationalisation

--- NumberOfComments.i18n.php
+++ NumberOfComments.i18n.php
@@ -7,17 +7,30 @@
 
 $messages = array();
 
-/** English */
+/** English (English)
+ * @author UltrasonicNXT
+ */
 $messages['en'] = array(
-	'numberofcomments-desc' => 'Provides a <nowiki>{{NoC}}</nowiki> magic word, for use with the Comments extension',
+	'numberofcomments-desc' => 'Provides a <nowiki>{{NoC}}</nowiki> magic word, for use with the Comments extension.',
 );
 
-/** Message documentation */
+/** German (Deutsch)
+ * @author ToaMeiko
+ */
+$messages['de'] = array(
+	'numberofcomments-desc' => 'Bietet ein Zauberwort für die Verwendung mit der Kommentare Verlängerung.'
+);
+
+/** Message documentation (Message documentation)
+ * @author UltrasonicNXT
+ */
 $messages['qqq'] = array(
 	'numberofcomments-desc' => '{{desc}}',
 );
 
-/** Vietnamese */
+/** Vietnamese (Việt)
+ * @author Codyn329
+ */
 $messages['vi'] = array (
 	'numberofcomments-desc' => 'Cung cấp một từ kỳ diệu được sử dụng với  ý kiến việc mở rộng.',
 );
